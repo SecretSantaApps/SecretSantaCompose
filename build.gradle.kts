054c84d488392aeb9281842349778491ea042d83
Vladislav Kheynov <lcastory86@gmail.com>
2023-04-11 00:49:24 +0300

    chore: add ktlint git hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,19 @@ buildscript {
         classpath("com.google.gms:google-services:4.3.15")
         val navVersion = "2.5.3"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
-
+        
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("com.google.firebase:perf-plugin:1.4.2")
     }
 }
+
+tasks.register("installGitHook", Copy::class) {
+    from(layout.projectDirectory.file("pre-commit"))
+    into(layout.projectDirectory.file(".git/hooks"))
+    fileMode = 0b0111101101
+}
+
+tasks.getByPath(":app:preBuild").dependsOn(tasks.getByName("installGitHook"))
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
